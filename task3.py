f928ef991bac696b98a42e663c02d36450ed7955
skrvferec <142224840+skrvferec@users.noreply.github.com>
2024-03-12 01:24:48 +0400

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,114 @@
+# tsk 1
+
+
+# products = []
+
+
+# for i in range(6):
+    
+#     name = input(f"{i+1}. məhsulun adını daxil edin: ")
+    
+#     price = float(input(f"{i+1}. məhsulun qiymətini daxil edin (AZN): "))
+    
+    
+#     product = (name, price)
+    
+#     products.append(product)
+
+
+# print("\\nMəhsulların siyahısı:")
+# for name, price in products:
+#     print(f"Məhsul: {name}, Qiymət: {price} AZN")
+
+# tsk3
+
+
+# patients = []
+
+# for i in range(5):
+   
+#     name = input(f"{i+1}. xəstənin adını daxil edin: ")
+    
+#     age = int(input(f"{i+1}. xəstənin yaşını daxil edin: "))
+    
+   
+#     patient = (name, age)
+    
+#     patients.append(patient)
+
+
+# print("\\nXəstələrin siyahısı:")
+# for name, age in patients:
+#     print(f"Xəstə: {name}, Yaş: {age}")
+
+# tsk4
+
+
+# students = []
+
+
+# for i in range(8):
+    
+#     name = input(f"{i+1}. tələbənin adını daxil edin: ")
+    
+#     age = int(input(f"{i+1}. tələbənin yaşını daxil edin: "))
+    
+    
+#     student = (name, age)
+    
+#     students.append(student)
+
+
+# print("\\nTələbələrin siyahısı:")
+# for name, age in students:
+#     print(f"Tələbə: {name}, Yaş: {age}")
+
+# tsk 5
+
+
+# foods = []
+
+
+# for i in range(4):
+    
+#     name = input(f"{i+1}. qida məhsulunun adını daxil edin: ")
+    
+#     price = float(input(f"{i+1}. qida məhsulunun qiymətini daxil edin (AZN): "))
+    
+    
+#     product = (name, price)
+   
+#     foods.append(product)
+
+
+# print("\\nQida məhsullarının siyahısı:")
+# for name, price in foods:
+#     print(f"Qida məhsulu: {name}, Qiymət: {price} AZN")
+
+
+
+
+# tsk6
+
+
+# software_developer = []
+
+
+# for i in range(4):
+    
+#     name = input(f"{i+1}. tərtibatçının adını daxil edin: ")
+    
+#     age = int(input(f"{i+1}. tərtibatçının yaşını daxil edin: "))
+
+#     language = input(f"{i+1}. tərtibatçının istifadə etdiyi proqramlaşdırma dilini daxil edin: ")
+    
+   
+#     software = (name, age, language)
+   
+#     software_developer.append(software)
+
+
+# print("\\nProqram tərtibatçılarının siyahısı:")
+# for ad, yas, dil in software_developer:
+#     print(f"Tərtibatçı: {name}, Yaş: {age}, Dil: {language}")
+
