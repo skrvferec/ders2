aaabdd61a57c6587c5132b7cfb1115687cc61cea
skrvferec <142224840+skrvferec@users.noreply.github.com>
2024-03-16 13:09:21 +0400

    Add files via upload

---
+++ pratik8.py
@@ -0,0 +1,74 @@
+# tsk 2
+
+# letter1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',]
+
+
+# letter2 = ['a', 'e', 'i', 'o', 'u']  
+
+
+# i = 0
+
+
+# while i < len(letter1):
+    
+#     if letter1[i] in letter2:
+#         i += 1
+#         continue
+    
+#     print(letter1[i])
+   
+#     i += 1
+
+# tsk1
+
+# data_list = ["alma", "banan",  "nar"]
+
+
+# i = 0
+
+
+# while i < len(data_list):
+#     print(data_list[i])
+#     i += 1
+
+# tsk3
+
+
+# num = int(input("Bir ədəd daxil edin: "))
+
+# i = 0
+
+
+# while i <= num:
+#     print(i)
+#     i += 1
+
+# tsk 4
+
+# static_parol = "Parol123"
+
+
+# chance = 3
+
+
+# for i in range(chance, 0, -1):
+    
+#     password = input("Parolu daxil edin: ")
+    
+    
+#     if password == static_parol:
+#         print("Parol doğrudur.")
+#         break
+#     else:
+        
+#         print(f"Səhv! {i-1} şansınız qaldı.")
+        
+        
+#         if i == 1:
+#             print("Bütün şanslarınız bitti, bloklandınız.")
+
+# tsk 5
+
+
+
+
